bca1eabe7997f69438c1442472e86d5aa380dd65
nafiuishaaq <nafiuig@gmail.com>
2026-03-02 16:55:27 +0100

    implemented the service

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [TypeOrmModule.forFeature([User]), NotificationsModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
